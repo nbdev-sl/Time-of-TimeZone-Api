53c70d6ca5bb5c7b3d922b434743ef8afe33e00f
Binura Chathunka <154507207+nbdev-sl@users.noreply.github.com>
2024-06-10 17:05:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 // Route to get the current time in Sri Lanka
-app.get('/', (req, res) => {
+app.get('/current-time', (req, res) => {
   try {
     // Get the current time in Sri Lanka
     const sriLankaTime = moment().tz('Asia/Colombo').format('YYYY-MM-DD HH:mm:ss');
@@ -27,6 +27,11 @@ app.get('/', (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
+});
+
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the Sri Lanka Time API. Access the current time at /current-time');
 });
 
 app.listen(PORT, () => {
